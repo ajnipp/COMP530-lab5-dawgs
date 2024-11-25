08a0a8c95ed31e8d9edb032488db25430ac62bf2
ajnipp <65468669+ajnipp@users.noreply.github.com>
2024-11-25 21:49:15 +0000

    initial commit

---
+++ microbenchmark.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void help() {
+  printf("Usage: microbenchmark [options]\n");
+  printf("Options:\n");
+  printf("  -c <num>  Number of clients\n");
+}
+
+int main(int argc, char **argv) {
+  int numthreads = 1; // default to 1
+  int c, i, rv;
+  pthread_t *tinfo;
+  long *opt_count;
+  struct timeval start, end;
+  long mtime, seconds, useconds;
+
+  // Read options from command line:
+  //   # clients from command line, as well as seed file
+  //   Simulation length
+  while ((c = getopt(argc, argv, "c:hl:s:")) != -1) {
+    switch (c) {
+    case 'c':
+      break;
+    case 'h':
+      return 0;
+    case 'l':
+    //   simulation_length = atoi(optarg);
+      break;
+    case 's':
+    //   global_salt = atoi(optarg);
+      break;
+    default:
+      printf("Unknown option\n");
+      help();
+      return 1;
+    }
+  }
+}
